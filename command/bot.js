f3d602a812f201ea144817d19a7389161f9efca0
Qasim & SuLiMaN <122628281+QXS-871@users.noreply.github.com>
2024-04-30 10:57:55 +0500

    Update bot.js

--- command/bot.js
+++ command/bot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Venocyber", "Venocyber1", "Venocyber2", "Venocyber3", "Venocyber4"])}.mp3`
+let av = `./Assets/mp3/${pickRandom(["SHaYan", "SHaYan1", "SHaYan2", "SHaYan3", "SHaYan4"])}.mp3`
 
 m.reply( `Hello ${taguser} Am Venocyber-md creatd by Venocyber tech Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(Venocyber|Team Venocyber)$/i
+handler.customPrefix = /^(SHaYan|Team SHaYan)$/i
 handler.command = new RegExp
 
 export default handler
